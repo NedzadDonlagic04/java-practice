614eb554dbcceb22218358dc813647e38cf7f8e1
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-09-18 13:40:17 +0200

    Finished my first java program

---
+++ 1/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello world!");
+        System.out.println("Hello, world!");
+
+        /*
+         * print and println both output text
+         * println just adds a newline at the end
+         */
+    }
+
+    // This is a comment
+
+    /*
+     * This is a multi line comment
+     */
+
+}
